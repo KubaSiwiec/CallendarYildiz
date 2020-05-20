bb5ca9ccbc5c0ecff262014a6729d81c7318d317
Jakub Siwiec <jakubsiwiec.nwm@gmail.com>
2020-05-20 21:59:25 +0200

    enable/disable radiobuttons if repeatswitchchecked

--- app/src/main/java/com/jakubsiwiec/callendar/SecondFragment.java
+++ app/src/main/java/com/jakubsiwiec/callendar/SecondFragment.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -29,6 +31,8 @@ public class SecondFragment<AddReminder> extends Fragment {
     public TextView TVfrom;
     public TextView TVto;
     public Button bDate;
+    public Switch swRepeat;
+    public RadioGroup rgRepeat;
     public EditText editTextName;
 
     //define date variables and set them to -1 before user sets custom time
@@ -67,6 +71,8 @@ public class SecondFragment<AddReminder> extends Fragment {
         TVfrom = (TextView) view.findViewById(R.id.textViewFrom);
         TVto = (TextView) view.findViewById(R.id.textViewTo);
         bDate = (Button) view.findViewById(R.id.buttonDate);
+        swRepeat = (Switch) view.findViewById(R.id.switchRepeat);
+        rgRepeat = (RadioGroup) view.findViewById(R.id.RBGroupRepeat);
         editTextName = (EditText) view.findViewById(R.id.editTextName);
 
 
@@ -158,14 +164,14 @@ public class SecondFragment<AddReminder> extends Fragment {
         /*
         Set date function
          */
-        view.findViewById(R.id.buttonDate).setOnClickListener(new View.OnClickListener() {
+        bDate.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
                 Calendar mcurrentDate = Calendar.getInstance();
                 int day = mcurrentDate.get(Calendar.DAY_OF_MONTH);
-                int month = mcurrentDate.get(Calendar.MONTH) + 1; //for some reason Calendar.MONTH returns value from 0 to 11
+                int month = mcurrentDate.get(Calendar.MONTH);
                 int year = mcurrentDate.get(Calendar.YEAR);
 
                 DatePickerDialog mDatePicker = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
@@ -173,7 +179,7 @@ public class SecondFragment<AddReminder> extends Fragment {
                     public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDayOfMonth) {
                         //set variables to to add them to database
                         evDay = selectedDayOfMonth;
-                        evMonth = selectedMonth;
+                        evMonth = selectedMonth + 1; //for some reason Calendar.MONTH returns value from 0 to 11
                         evYear = selectedYear;
                         //print chosen date on the button
                         String dateChosen = "Date: " + selectedYear + ":" + selectedMonth + ":" + selectedDayOfMonth;
@@ -183,6 +189,28 @@ public class SecondFragment<AddReminder> extends Fragment {
 
                 mDatePicker.setTitle("Select Date");
                 mDatePicker.show();
+
+            }
+        });
+
+        /*
+        Enable or disable repeat
+         */
+        swRepeat.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //if repeat is not checked
+                if (!swRepeat.isChecked()){
+                    for (int i = 0; i < rgRepeat.getChildCount(); i++){
+                        rgRepeat.getChildAt(i).setEnabled(false);
+                    }
+                } else {
+                    for (int i = 0; i < rgRepeat.getChildCount(); i++){
+                        rgRepeat.getChildAt(i).setEnabled(true);
+                    }
+                }
 
             }
         });
