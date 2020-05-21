04df78a3fda3a17cff4eae9856d4a0adfa0a0898
Jakub Siwiec <jakubsiwiec.nwm@gmail.com>
2020-05-21 13:29:51 +0200

    get data from fields when save button pressed

--- app/src/main/java/com/jakubsiwiec/callendar/SecondFragment.java
+++ app/src/main/java/com/jakubsiwiec/callendar/SecondFragment.java
@@ -8,8 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -34,10 +36,25 @@ public class SecondFragment<AddReminder> extends Fragment {
     public Switch swRepeat;
     public RadioGroup rgRepeat;
     public EditText editTextName;
+    public EditText editTextDetails;
+    public EditText editTextLocation;
+    public RadioButton rbChecked;
+    public CheckBox cb5min, cb15min, cb1h, cb6h, cb1d;
 
     //define date variables and set them to -1 before user sets custom time
     private int finishHour = -1, finishMinute = -1, evDay = -1, evMonth = -1, evYear = -1, startMinute = -1, startHour = -1;
 
+    //declare String variables
+    private String evName, evDetails, evLocation;
+
+    //repeat or not bool
+    private boolean evRepeat = false;
+
+    //when to repeat
+    private int evHowOftenRepeat = -1;
+
+    //when to remind bools
+    private boolean remind5min, remind15min, remind1h, remind6h, remind1d;
 
 
 
@@ -68,12 +85,22 @@ public class SecondFragment<AddReminder> extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //View objects definition to access their properties
         TVfrom = (TextView) view.findViewById(R.id.textViewFrom);
         TVto = (TextView) view.findViewById(R.id.textViewTo);
         bDate = (Button) view.findViewById(R.id.buttonDate);
         swRepeat = (Switch) view.findViewById(R.id.switchRepeat);
         rgRepeat = (RadioGroup) view.findViewById(R.id.RBGroupRepeat);
         editTextName = (EditText) view.findViewById(R.id.editTextName);
+        editTextDetails = (EditText) view.findViewById(R.id.editTextDetails);
+        editTextLocation = (EditText) view.findViewById(R.id.editTextLocation);
+        cb5min = (CheckBox) view.findViewById(R.id.checkBox5m);
+        cb15min = (CheckBox) view.findViewById(R.id.checkBox15m);
+        cb1h = (CheckBox) view.findViewById(R.id.checkBox1h);
+        cb6h = (CheckBox) view.findViewById(R.id.checkBox6h);
+        cb1d = (CheckBox) view.findViewById(R.id.checkBox1d);
+
+
 
 
         /*
@@ -83,12 +110,50 @@ public class SecondFragment<AddReminder> extends Fragment {
             @Override
             public void onClick(View view) {
 
-                String evName = editTextName.getText().toString();
+                evName = editTextName.getText().toString();
 
                 if (checkIfDateTimeNotSet(evYear, evMonth, evDay, startHour, startMinute, finishHour, finishMinute) || evName.equals("")){
                     Toast.makeText(getContext(), "Before saving event set it's name, date, and time", Toast.LENGTH_LONG).show();
                 }
                 else{
+
+                    //Trzeba zapisać zmienne do bazy danych
+                    //Inne funkcje w tym fragmencie ogarniają zapis daty i czasu do zmiennych
+                    //Są one w zmiennych: evYear, evMonth, evDay, startHour, startMinute, finishHour, finishMinute
+                    //Trzeba je tu zqapisać w bazie
+                    //Nazwa jest definiowana wyżej w tej funkcji jako evName
+                    //Reszta zmiennych: opis, lokację, powtarzanie i remindery będą zdefiniowane tu i później zapisane do bazy
+
+                    //details and location
+                    evDetails = editTextDetails.getText().toString();
+                    evLocation = editTextLocation.getText().toString();
+
+                    //repeat - is true when user checked repeat switch
+                    evRepeat = swRepeat.isChecked();
+
+                    //how often to repeat
+                    //tags: 1 for weekly, 2 for monthly, 3 for yearly
+                    //iterate through radio buttons to see which one is selected
+                    Log.i("Selected", String.valueOf(rgRepeat.getCheckedRadioButtonId()));
+                    rbChecked = (RadioButton) rgRepeat.findViewById(rgRepeat.getCheckedRadioButtonId());
+                    evHowOftenRepeat = Integer.parseInt((String) rbChecked.getTag());
+
+                    //when to remind the user - every checkbox is treaten separately
+                    //later this datastructure may be changed to array of bools
+                    remind5min = cb5min.isChecked();
+                    remind15min = cb15min.isChecked();
+                    remind1h = cb1h.isChecked();
+                    remind6h = cb6h.isChecked();
+                    remind1d = cb1d.isChecked();
+
+                    Log.i("Name", evName);
+                    Log.i("Details", evDetails);
+                    Log.i("Location", evLocation);
+                    Log.i("DateTime", String.valueOf(evYear) + "." + String.valueOf(evMonth) + "." + String.valueOf(evDay) + "  From: " + String.valueOf(startHour) + ":" + String.valueOf(startMinute) + " to: " + String.valueOf(finishHour) + ":" + String.valueOf(finishMinute));
+                    Log.i("Repeat", String.valueOf(evRepeat));
+                    Log.i("How often to repeat", String.valueOf(evHowOftenRepeat));
+                    Log.i("Remind", "5min:" + String.valueOf(remind5min) + ", 15 min:" + String.valueOf(remind15min) + ", 1h: " + String.valueOf(remind1h) + ", 6h: " + String.valueOf(remind6h) + ", 1day: " + String.valueOf(remind1d));
+
 
                     /*
                     Zapis do bazy danych
