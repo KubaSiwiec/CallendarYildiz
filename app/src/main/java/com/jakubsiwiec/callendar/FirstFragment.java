7ef977c5e2d3c0ef49de7ee497190ddb3afffa48
Jakub Siwiec <jakubsiwiec.nwm@gmail.com>
2020-06-07 21:09:00 +0200

    Display events list view

--- app/src/main/java/com/jakubsiwiec/callendar/FirstFragment.java
+++ app/src/main/java/com/jakubsiwiec/callendar/FirstFragment.java
@@ -1,13 +1,18 @@
 package com.jakubsiwiec.callendar;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -18,19 +23,52 @@ import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
 
+
+    /*
+    Variables
+     */
+    private static final String TAG = "ListDataFragment";
     private DataBaseHelper dataBaseHelper;
+    private ListView listViewEvents;
+
+
+    /*
+    Functions
+     */
+    private void populateListView(){
+        Log.d(TAG, "Populate list view: displaying data in the ListView");
+        Cursor data = dataBaseHelper.getData();
+        ArrayList<String> listData = new ArrayList<>();
+        while(data.moveToNext()){
+            listData.add(data.getString(1));
+            Log.d(TAG, data.getString(1));
+        }
+
+        Log.d(TAG, String.valueOf(data.getCount()));
+
+        ListAdapter adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, listData);
+        listViewEvents.setAdapter(adapter);
+
+    }
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        listViewEvents = (ListView) view.findViewById(R.id.listViewEvents);
+        dataBaseHelper = new DataBaseHelper(getContext());
+
+        populateListView();
 
 
         view.findViewById(R.id.button_add_event).setOnClickListener(new View.OnClickListener() {
